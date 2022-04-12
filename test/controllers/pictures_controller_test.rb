6b1f1150682811280a25b2c0d44125f61cd75f63
Vitali <you@example.com>
2022-04-12 16:15:41 +0300

    Last commit)

---
+++ test/controllers/pictures_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class PicturesControllerTest < ActionDispatch::IntegrationTest
+  test "should get crud" do
+    get pictures_crud_url
+    assert_response :success
+  end
+end
